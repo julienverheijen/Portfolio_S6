1bf8db0d866b625b60985da806029c11d468b860
Verheijen,Julien J.B.R <j.verheijen@student.fontys.nl>
2023-10-02 22:01:29 +0200

    Navigation link fixed

---
+++ readingguide.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reading Guide</title>
+    <script type="module" src="script.js"></script>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body class="maxViewWidth">
+    
+<?php include("navigation.html");?>
+
+</body>
+</html>
