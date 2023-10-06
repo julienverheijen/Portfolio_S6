81fb03b36c7ada9a1274818838fa67f1996cc812
Verheijen,Julien J.B.R <j.verheijen@student.fontys.nl>
2023-10-06 15:23:19 +0200

    Content added

--- deliverbles/research_navigation.php
+++ deliverbles/research_navigation.php
@@ -29,6 +29,58 @@
                 <article class="body_results">
                     <h2>RESULTS</h2>
                     <p class="results_text" id="deliverable-results-text"></p>
+                        <p>The following questions must be answered first, to answer the research question ("How can users easily navigate through the portfolio?").</p>
+                        <ul>
+                        <li>Who is going to use to portfolio?</li>
+                        <li>What kind of navigations do exist?</li>
+                        <li>What should be included in the navigation menu?</li>
+                        <li>Which navigation type will work best for the portfolio?</li>
+                        </ul>
+                        <p>&nbsp;</p>
+                        <p><strong>Who is going to use the portfolio?</strong></p>
+                        <p>This portfolio will be specifically used for presenting school projects, so it will only be viewed by Fontys ICT teachers and students from Eindhoven. This small target group has above-average technical skills and can navigate web pages without difficulty. No additional explanation is required for using the website.</p>
+                        <p>Everyone in this target group can read English, so the portfolio does not have to be created in multiple languages.</p>
+                        <p>Target group:<em> Above-average technical, English reading between 20 and 40 years old.</em></p>
+                        <p><em>&nbsp;</em></p>
+                        <p><strong>What kind of navigations do exist?</strong></p>
+                        <p>An article by Fitzgerald (2023) and Bąk (2023) explains the different types of navigation menus. The differences are explained below:</p>
+                        <p><em>Horizontal Navigation Bar</em></p>
+                        <p>The most common type, list pages side-by-side and place them in the website header. This menu bar can be sticky or fixed when the user scrolls through the page the menu bar stays visible on top of the content.&nbsp;</p>
+                        <p><em>Dropdown Navigation Menu</em></p>
+                        <p>Ideal for sites with a lot of different pages, you can list multiple links under the same navigation button and only show one category name.</p>
+                        <p><em>Hamburger Navigation Menu</em></p>
+                        <p>Most popular navigation for small screen sizes, such as mobile devices. The menu items will collapse behind a hamburger (three-lines) icon. The menu items will be visible after clicking on the icon.</p>
+                        <p><em>Vertical Sidebar Navigation Menu</em></p>
+                        <p>This navigation bar offers a seamless user experience, Admittedly, this is less popular than horizontal navigation, but vertical navigation has benefits. The space isn&rsquo;t that limited so you can write longer navigation links. There's also the opportunity for more top-level options.</p>
+                        <p><em>Footer Navigation Menu</em></p>
+                        <p>A footer menu is on the bottom of the page and is mostly paired with a horizontal navigation bar. If visitors don't see the link they need in the header, they can scroll down to the bottom of the page where they'll find more options. The footer menu is not the primary navigation menu on a website but rather a secondary way for users to navigate the site.</p>
+                        <p><em>Breadcrumbs Navigation Menu</em></p>
+                        <p>Breadcrumbs are mostly displayed in a horizontal list. Visitors can visualize where they are in the website&rsquo;s structure. They can easily go back multiple pages.</p>
+                        <p><em>Full-screen navigation menu</em></p>
+                        <p>This menu is becoming more popular among web developers. It is a full-screen overlay of menu options that can be visible after clicking on a hamburger icon. This style is more eye-catching than the other menu alternatives. (Fitzgerald, 2023; Bąk, 2023)</p>
+                        <p><strong>&nbsp;</strong><strong>&nbsp;</strong></p>
+                        <p><strong>What should be included in the navigation menu?</strong></p>
+                        <p>According to Fitzgerald's (2023) article, should the navigation include the objects that visitors are looking for and the goals I want to achieve with the portfolio.</p>
+                        <p>After a conversation with a user from the target group, I concluded that the menu does not need to be extensive and there does not need to be a mandatory 'about me' page or 'reading guide'. (B. Dennen, personal communication, September 13, 2023)</p>
+                        <p>I made the following navigation structure to get a clear view of the pages that I will be including in the portfolio.</p>
+                        <p>You can see that the portfolio consists of three separate pages (Home, Products, Reading Guide). With multiple sub-project pages.</p>
+                        <p>&nbsp;<strong>&nbsp;</strong></p>
+                        <p><strong>Which navigation type will work best for the portfolio?</strong></p>
+                        <p>To find out, the advantages and disadvantages of each type of navigation will have to be examined. The needs of the target group will also have to be considered.</p>
+                        <p><em>Horizontal Navigation Bar</em></p>
+                        <p>This type of navigation is easy to identify and navigate. Because this is the most common type of menu, users will be comfortable using it. With a fixed version it can be annoying for the user to scroll back to the top of the page. A sticky menu can fill up a lot of space but is always visible.</p>
+                        <p><em>Dropdown Navigation Menu</em></p>
+                        <p>This type of navigation is not ideal for this project, because there are not enough page categories.&nbsp;</p>
+                        <p><em>Hamburger Navigation Menu</em></p>
+                        <p>This type of navigation is recommended for small screens, most users for this portfolio will be desktop users. The menu is not always visible, this provides an extra click to open the menu.</p>
+                        <p><em>Vertical Sidebar Navigation Menu</em></p>
+                        <p>This type of navigation saves up space if you use it with a hamburger button, but this provides an extra click to open.<em>&nbsp;</em></p>
+                        <p><em>Footer Navigation Menu</em></p>
+                        <p>This type of navigation will only work as secondary navigation. This is not needed in a portfolio like this.</p>
+                        <p><em>Breadcrumbs Navigation Menu</em></p>
+                        <p>This type of navigation will give a good overview of the structure of the portfolio. But it only makes sense if there are more than three referrals in the navigation menu. This will not be the case in this portfolio.</p>
+                        <p><em>Full-screen navigation menu</em></p>
+                        <p>This type of navigation will stand out from the traditional ones and can be, more creative with the maximum space to use for the navigation. However, it can be overwhelming or confusing, especially for users in an older demographic.</p>
                 </article>
 
                     <div class="body_results_img">
