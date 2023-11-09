690ea9924d4ea3aba9d00f97a6ef70bc174742b8
Verheijen,Julien J.B.R <j.verheijen@student.fontys.nl>
2023-11-09 12:17:23 +0100

    Image Loader added

--- projects.js
+++ projects.js
@@ -5,18 +5,27 @@ for (let i = 0; i < 10; i++) {
   container.append(cardTemplate.content.cloneNode(true));
 }
 
-fetch("data.json")
-  .then((response) => response.json())
-  .then((posts) => {
-    container.innerHTML = "";
-    posts.forEach((post) => {
-      const div = cardTemplate.content.cloneNode(true);
-      div.getElementById("card-link").href = post.link;
-      div.getElementById("card-projectType-icon").src = post.projectTypeIcon;
-   //    div.getElementById("card-projectType-icon").content = post.learningOutcomeIcon;
-      div.getElementById("card-title").textContent = post.title;
-      div.getElementById("card-details").textContent = post.context;
-      div.getElementById("card-cover-img").src = post.coverImage;
-      container.append(div);
-    });
-  });
+
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    console.log('loaded')
+    setTimeout(() => {
+      fetch("data.json")
+      .then((response) => response.json())
+      .then((posts) => {
+        container.innerHTML = "";
+        posts.forEach((post) => {
+          const div = cardTemplate.content.cloneNode(true);
+          div.getElementById("card-link").href = post.link;
+          div.getElementById("card-projectType-icon").src = post.projectTypeIcon;
+       //    div.getElementById("card-projectType-icon").content = post.learningOutcomeIcon;
+          div.getElementById("card-title").textContent = post.title;
+          div.getElementById("card-details").textContent = post.context;
+          div.getElementById("card-cover-img").src = post.coverImage;
+          container.append(div);
+        });
+      });
+    
+    }, 0); // 0 is geen vertraging. ms (1000 = 1 sec)
+  })
